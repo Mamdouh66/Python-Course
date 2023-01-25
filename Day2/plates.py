6d9cdb77bd7ad94a67f00ca639a1892277df2cf9
Mamdouh66 <MamdouhKhalaf76@gmail.com>
2023-01-25 09:15:49 +0300

    solved plates

--- Day2/plates.py
+++ Day2/plates.py
@@ -7,17 +7,32 @@ def main():
 
 
 def is_valid(s):
-    for i in range(len(s)):
-        if s[i] in (".", " ", ","):
-            return False
-        if s[i].isdigit():
-            digits = s[i].rsplit()
-            for i in range(len(digits)):
-                if not int(digits[i]) >= 0 and int(digits[i]) <= 9:
+    if len(s) >= 2 and len(s) <= 6:
+        if s[0].isalpha() and s[1].isalpha():
+            for i in range(len(s)):
+                digitsInTheMiddle = []
+                digitsInTheMiddle = s[1:-1]
+                if digitsInTheMiddle.isnumeric() and s[-1].isalpha():
                     return False
-        if not s[0].islower() and s[1].islower():
-            return False
-    return True
+                if s[i].isdigit() and s[i] != "0":
+                    lastDigits = []
+                    lastDigits = s[i:]
+                    for j in range(len(lastDigits)):
+                        if not lastDigits[j].isdigit():
+                            return False
+                    if not s.isalnum():
+                        return False
+                    else:
+                        return True
+                elif s[i] == "0":
+                    return False
+            if not s.isalnum():
+                return False
+            else:
+                return True
+
+    else:
+        return False
 
 
 main()
